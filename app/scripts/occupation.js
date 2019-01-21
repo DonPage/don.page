f4eeef8f8e917e3144aaff4c6371ea6ad61bc181
donpage <don.page.work@gmail.com>
2019-01-21 15:01:10 -0800

    god damn dom.

--- app/scripts/occupation.js
+++ app/scripts/occupation.js
@@ -1,22 +1,20 @@
 /**
  * @function occupation
  * @description - this function handles the occupation input.
- * @param {HTMLElement} input - canvas element.
- * @return {Object}
+ * @param {HTMLElement} element - input element.
  */
 const occupation = (element) => {
-  const config = {attributes: true, childList: false, subtree: false};
-  // TODO: do mutation observer stuff.
   const input = {
     value: element.value,
     actions: {
       setValue: (value) => {
         input.value = value;
+        element.setAttribute('value', value);
       }
     },
     subscriptions: {
       pageTitle: (newValue) => {
-        document.title = newValue;
+        document.title = `Don.Page - ${newValue}`;
       },
       log: (newValue) => {
         console.log(`%c ${newValue} `, 'background: #222; color: #fff;  padding: 5px;');
@@ -27,5 +25,22 @@ const occupation = (element) => {
     }
   };
 
-  return input
+  const observer = new MutationObserver((mutationsList) => {
+    for (const mutation of mutationsList) {
+      if (mutation.type === 'attributes' && mutation.attributeName === 'value') {
+        Object.keys(input.subscriptions).map(key => {
+          input.subscriptions[key].call(this, element.value)
+        })
+      }
+    }
+  });
+
+  observer.observe(element, {attributes: true, childList: false, subtree: false});
+
+  element.addEventListener('input', (e) => {
+    input.actions.setValue(e.target.value)
+  });
+
+  const defaultOccupation = localStorage.getItem('occupation') || 'Software Engineer';
+  input.actions.setValue(defaultOccupation);
 };
